827dac5b4d912650b8613802c32b0d477f40b28c
ogatu <to3368@gmail.com>
2020-11-06 17:58:41 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/tsuyoshi/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package jp.techacademy.tsuyoshi.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+    }
+
+    override fun onClick(v:View){
+
+　　Log.d("UI_PARTS", "ボタンをタップしました")
+  }
+}
+
+
+
+
